999dab50b70d78950d59cef88856fc47c0e304d2
Shelton Gayles <sgbythelake@gmail.com>
2018-08-27 15:56:23 -0700

    added pretty error handling

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask import abort, Flask, render_template
 
 
 # local imports
@@ -36,6 +37,24 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint)
 
     from .home import home as home_blueprint
-    app.register_blueprint(home_blueprint) 
+    app.register_blueprint(home_blueprint)
+
+    # Test for 500 error
+    # @app.route('/500')
+    # def error():
+    #     abort(500)
+
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title='Forbidden'), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title='Page Not Found'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title='Server Error'), 500 
 
     return app
