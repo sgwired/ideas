4256fafa776ef564d0d92b82290a475b92788fbd
Shelton Gayles <sgbythelake@gmail.com>
2018-08-27 23:31:33 -0700

    added privacy terms etc to main page

--- app/home/views.py
+++ app/home/views.py
@@ -12,6 +12,22 @@ def homepage():
     return render_template('home/index.html', title="Welcome")
 
 
+@home.route('/terms')
+def terms():
+    """
+    Render the homepage template on the / route
+    """
+    return render_template('home/terms.html', title="Terms Of Service")
+
+
+@home.route('/privacy')
+def privacy():
+    """
+    Render the homepage template on the / route
+    """
+    return render_template('home/privacy.html', title="Privacy Policy")
+
+
 @home.route('/dashboard')
 @login_required
 def dashboard():
@@ -19,6 +35,7 @@ def dashboard():
     Render the dashboard template on the /dashboard route
     """
     return render_template('home/dashboard.html', title="Dashboard")
+
 
 # add admin dashboard view
 @home.route('/admin/dashboard')
