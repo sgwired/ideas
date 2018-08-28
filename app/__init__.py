4b887a894f2f1ded89b2e6c222e1b6f673d40444
Shelton Gayles <sgbythelake@gmail.com>
2018-08-27 18:44:07 -0700

    modifications for live deploy

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask import abort, Flask, render_template
-
+import os
 
 # local imports
 from config import app_config
@@ -16,9 +16,16 @@ login_manager = LoginManager()
 
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
 
     Bootstrap(app)
     db.init_app(app)
