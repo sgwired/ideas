f6a3323b1787089737f03805916ce8f50f720520
U-JAKKSPACIFICINC\sheltong <sheltong@OCEAN-4358.jakkspacificinc.local>
2018-08-28 19:01:32 -0700

    fixed the crud idea complete issue

--- app/home/views.py
+++ app/home/views.py
@@ -1,4 +1,4 @@
-from flask import abort, render_template, redirect
+from flask import abort, render_template, redirect, url_for, flash
 from flask_login import current_user, login_required
 
 from . import home
